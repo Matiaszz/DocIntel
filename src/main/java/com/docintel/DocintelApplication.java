f4b5ccd53ec011af1e24bc64f172ca25f759f351
Allan Matias <alangiovanepaes@gmail.com>
2026-06-29 14:34:41 -0300

    chore: init

---
+++ src/main/java/com/docintel/DocintelApplication.java
@@ -0,0 +1,13 @@
+package com.docintel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocintelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocintelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/docintel/DocintelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docintel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocintelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
